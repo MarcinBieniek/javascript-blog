740c476fc077b693d13f197a5152890fa4fddef1
Marcin Bieniek <marcingim@gmail.com>
2022-03-30 14:11:18 +0200

    Add calculateTagsParams function

--- js/script.js
+++ js/script.js
@@ -108,7 +108,24 @@ const generateTitleLinks = function(customSelector = '') {
 
 generateTitleLinks();
 
-// [step 3 / step 7 NEW]
+// [step 3 / step 7 NEW / step 8 - add calculateTagsParams function]
+
+// [IN PROGRESS]
+
+const calculateTagsParams = function(tags){
+
+  const params = {max: 0, min: 999999};
+
+    for(let tag in tags){
+        params.max = Math.max(tags[tag], params.max);
+        params.min = Math.min(tags[tag], params.min);
+    }
+
+  return params;
+
+}
+
+
 
 function generateTags(){
 
@@ -130,8 +147,6 @@ function generateTags(){
     const tagsWrapper = article.querySelector(optArticleTagsSelector);
 
     /* [DONE] make html variable with empty string */
-
-
 
     /* [DONE] get tags from data-tags attribute */
 
@@ -176,12 +191,16 @@ function generateTags(){
   /* [NEW] find list of tags in right column */
   const tagList = document.querySelector(optTagsListSelector);
 
+  const tagsParams = calculateTagsParams(allTags);
+  console.log('tagsParams:', tagsParams)
+
   /* [NEW] create variable for all links HTML code */
   let allTagsHTML = '';
 
   /* [NEW] START LOOP: for each tag in allTags: */
     for(let tag in allTags){
     /* [NEW] generate code of a link and add it to allTagsHTML */
+
       allTagsHTML += '<li><a href="#tag-' + tag + '">' + tag + ' (' + allTags[tag] + ')  ' + '</a></li>';
 
     /* [NEW] END LOOP: for each tag in allTags: */
@@ -190,8 +209,6 @@ function generateTags(){
 
   /*[NEW] add HTML from allTagsHTML to tagList */
   tagList.innerHTML = allTagsHTML;
-
-    console.log('alltagshtml: ' + allTagsHTML)
 
 }
 
